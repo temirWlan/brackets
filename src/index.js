8b698e4c0f35e2b170bdb0c9b92c50fd2afa32fe
Temirlan <temirlan.balguzhin@bk.ru>
2020-10-22 21:34:13 +0600

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let someStr = ' ';
+  
+  while (str.length) {
+    for (let bracket of bracketsConfig) {
+      const configStr = bracket.join('');
+      str = str.replace(configStr, '');
+    }
+    
+    if (someStr === str) break;
+    
+    someStr = str;
+  }
+  
+  return str.length === 0;
 }
+
+
